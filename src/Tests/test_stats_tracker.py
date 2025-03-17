a386db370d37fad10a871c918c9c0c8f1b4653de
Ian Ferguson <ian@Ian-Fergusons-MacBook-Air.local>
2025-03-17 20:58:47 +1100

    added more tests

---
+++ src/Tests/test_stats_tracker.py
@@ -0,0 +1,26 @@
+import pytest
+from src.stats_tracker import StatsTracker
+
+@pytest.fixture
+def stats_tracker():
+    return StatsTracker()
+
+def test_unique_ip_addresses_input(stats_tracker):
+    stats_tracker.unique_ip_addresses_input("192.168.1.1")
+    stats_tracker.unique_ip_addresses_input("192.168.1.2")
+    stats_tracker.unique_ip_addresses_input("192.168.1.1")  # Duplicate IP
+    assert stats_tracker.unique_IP_addresses_analyse() == 2
+
+def test_most_visited_urls_input(stats_tracker):
+    stats_tracker.most_visited_urls_input("http://example.com")
+    stats_tracker.most_visited_urls_input("http://example.com")
+    stats_tracker.most_visited_urls_input("http://example.org")
+    top_three = stats_tracker.most_visited_urls_analyse()
+    assert top_three == [("http://example.com", 2), ("http://example.org", 1)]
+
+def test_most_active_ip_addresses_input(stats_tracker):
+    stats_tracker.most_active_ip_addresses_input("192.168.1.1")
+    stats_tracker.most_active_ip_addresses_input("192.168.1.1")
+    stats_tracker.most_active_ip_addresses_input("192.168.1.2")
+    top_three = stats_tracker.most_active_ip_addresses_analyse()
+    assert top_three == [("192.168.1.1", 2), ("192.168.1.2", 1)]
